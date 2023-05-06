594f3afb645fc82d9a4844ec515ae75dd77a87a6
solaris0051 <30798030+solaris0051@users.noreply.github.com>
2023-05-06 17:31:30 +0900

    align app.js to make it easy to see.

--- app.js
+++ app.js
@@ -1,46 +1,41 @@
-const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const catalogRouter = require("./routes/catalog");
-
-const compression = require("compression");
-const helmet = require("helmet");
-
 const app = express();
-
-const RateLimit = require("express-rate-limit");
-const limiter = RateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 100,
-});
-
-app.use(limiter);
-
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-
-const dev_db_url =
-  "mongodb+srv://docodoco0713:0Y8nczZBMCaiUe40@cluster0.iki2ezu.mongodb.net/local_library?retryWrites=true&w=majority";
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
-
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
-}
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+const catalogRouter = require("./routes/catalog");
+app.use("/catalog", catalogRouter);
+
+const path = require("path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+const createError = require("http-errors");
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+const cookieParser = require("cookie-parser");
 app.use(cookieParser());
+
+const logger = require("morgan");
+app.use(logger("dev"));
+
+const compression = require("compression");
 app.use(compression());
+
+const helmet = require("helmet");
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
@@ -49,21 +44,21 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/catalog", catalogRouter);
-
-app.use(function (req, res, next) {
-  next(createError(404));
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 100,
 });
+app.use(limiter);
 
-app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.status(err.status || 500);
-  res.render("error");
-});
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const dev_db_url =
+  "mongodb+srv://docodoco0713:0Y8nczZBMCaiUe40@cluster0.iki2ezu.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+main().catch((err) => console.log(err));
 
 module.exports = app;
