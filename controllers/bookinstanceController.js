6432279a67411dba10c0cc22257d204bf31a67f6
solaris0051 <30798030+solaris0051@users.noreply.github.com>
2023-05-07 08:45:10 +0900

    書誌情報 <= 書籍名

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -22,7 +22,7 @@ exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
     return next(err);
   }
   res.render("bookinstance_detail", {
-    title: "書籍名:",
+    title: "書誌情報:",
     bookinstance: bookInstance,
   });
 });
@@ -36,7 +36,7 @@ exports.bookinstance_create_get = asyncHandler(async (req, res, next) => {
 });
 
 exports.bookinstance_create_post = [
-  body("book", "書籍名を指定してください").trim().isLength({ min: 1 }).escape(),
+  body("book", "書誌情報を指定してください").trim().isLength({ min: 1 }).escape(),
   body("imprint", "版を指定してください")
     .trim()
     .isLength({ min: 1 })
@@ -108,7 +108,7 @@ exports.bookinstance_update_get = asyncHandler(async (req, res, next) => {
 });
 
 exports.bookinstance_update_post = [
-  body("book", "書籍名を指定してください。").trim().isLength({ min: 1 }).escape(),
+  body("book", "書誌情報を指定してください。").trim().isLength({ min: 1 }).escape(),
   body("imprint", "版を指定してください。")
     .trim()
     .isLength({ min: 1 })
